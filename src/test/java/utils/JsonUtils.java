65bcf7c39b4a08911173a72fbbf591e85719f549
RafaelLeonardo18 <rafaelldl.rldl@gmail.com>
2021-11-18 23:12:47 -0300

    Adição de método dinâmico para alterar conteúdo JSON

--- src/test/java/utils/JsonUtils.java
+++ src/test/java/utils/JsonUtils.java
@@ -2,6 +2,8 @@ package utils;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Iterator;
+
 import org.json.JSONObject;
 
 public class JsonUtils {
@@ -10,4 +12,33 @@ public class JsonUtils {
         String content = new String(Files.readAllBytes(Paths.get("src/test/resources/json_files/" + jsonFile + ".json")));
         return new JSONObject(content);
     }
+
+    public static JSONObject updateJsonObject(JSONObject json, String keyMain, String newValue) throws Exception {
+        Iterator iterator = json.keys();
+        iterator.forEachRemaining(iteratorKey -> {
+            if ((json.optJSONArray(iteratorKey.toString()) == null) && (json.optJSONObject(iteratorKey.toString()) == null)) {
+                if (iteratorKey.toString().equals(keyMain)){
+                    json.put(iteratorKey.toString(), newValue);
+                }
+            }
+            if (json.optJSONObject(iteratorKey.toString()) != null) {
+                try {
+                    updateJsonObject(json.getJSONObject(iteratorKey.toString()), keyMain, newValue);
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+            if (json.optJSONArray(iteratorKey.toString()) != null){
+                json.optJSONArray(iteratorKey.toString()).forEach(jsonObject -> {
+                    try {
+                        updateJsonObject((JSONObject) jsonObject, keyMain, newValue);
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+                });
+            }
+        });
+        return json;
+    }
+
 }
